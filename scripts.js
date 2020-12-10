e3ca7016045ef48de4ce69a44f382813a54201b1
Paul Martin <paulmartin91@hotmail.co.uk>
2020-12-10 17:26:28 +0000

    added seperate blog page

--- scripts.js
+++ scripts.js
@@ -1,29 +1,7 @@
 var blog_data = {}
+const appendLeadingZeroes = (n) => n <= 9 ? "0" + n : n
 
 $(document).ready(function(){
-    /*
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-
-        // Store hash
-        var hash = this.hash;
-        var scrollPosition = $(hash).offset().top - 200//document.getElementById('nav-bar').offsetHeight
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: scrollPosition
-        }, 500, function(){
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
-        */
 
        $('body').on('click','a[href^="#"]',function(event){
             event.preventDefault();
@@ -36,9 +14,6 @@ $(document).ready(function(){
         // API to Integrate Blog
         var API_KEY = "AIzaSyBWqd6wzwcFgKaxC5BkNLYReXsZZqmUkk8"
         var bloggerID = "4759296590722902555" //"2399953" // "1330659412086602715" //
-        const appendLeadingZeroes = (n) => n <= 9 ? "0" + n : n
-
-        
 
         fetch(`https://www.googleapis.com/blogger/v3/blogs/${bloggerID}/posts/?key=${API_KEY}`)//, {mode: "no-cors"})
             .then(response => response.json())
@@ -49,23 +24,47 @@ $(document).ready(function(){
                 let temp_date = new Date(item.published)
                 let formatted_date = appendLeadingZeroes(temp_date.getDate()) + "/" + appendLeadingZeroes(temp_date.getMonth() + 1) + "/" + temp_date.getFullYear()
                 console.log(temp_date)
-                $(`#blog-container`).append(`
-                    <h5 id="main-blog-title" >${item.title} <span class="blog-date"><p>${formatted_date}</p></span></h5>
-                    <p id="main-blog-content" >${item.content}</p>
-                    <hr color="white">
-                `)
-                //if (index == 0) {
-                    // $('#main-blog-title').text(item.title)
-                    // $('#main-blog-content').html(item.content)
-                //}
-                //$('#sub-blog-tabs').append(
+                // $(`#blog-container`).append(`
+                //     <h5 id="main-blog-title" >${item.title} <span class="blog-date"><p>${formatted_date}</p></span></h5>
+                //     <p id="main-blog-content" >${item.content}</p>
+                //     <hr color="white">
+                // `)
+                if (index == 0) {
+                    $(`#blog-container`).append(`
+                        <h5 id="main-blog-title" >${item.title} <span class="blog-date"><p>${formatted_date}</p></span></h5>
+                        <p id="main-blog-content" >${item.content}</p>
+                    `)
+                }
+                $('#sub-blog-tabs').append(
                     // `
-                    //     <div onclick="selectBlog('${item.title}')" class="row sub-blog-tab">
-                    //         <a>${item.published}</a>
+                    //     <li onclick="selectBlog('${item.title}')" class="list-group-item">
+                    //         <a>${formatted_date}</a>
                     //         <h5>${item.title}</h5>
-                    //     </div>
+                    //     </li>
                     // `
-                //)
+                    // `
+                    // <div onclick="selectBlog('${item.title}')" class="card bg-light mb-2" style="max-width: 18rem; color: black">
+                    //     <div class="card-body">
+                    //     <span class="card-subtitle">  ${formatted_date}</span>
+                    //     <p class="card-title">${item.title}</p>
+                    //   </div>
+                    // </div>
+                    // `
+                    `
+                    <li onclick="selectBlog('${item.title}')" data-toggle="collapse" data-target="#navbarSupportedContent" class="list-group-item list-group-item-action" style="max-width: 18rem; color: black">
+                        <span class="card-subtitle">  ${formatted_date}</span>
+                        <p class="card-title">${item.title}</p>
+                    </li>
+                    `
+                )
+                $('#sub-blog-tabs-expanded').append(
+                    `
+                    <li onclick="selectBlog('${item.title}')" class="list-group-item list-group-item-action" style="max-width: 18rem; color: black">
+                        <span class="card-subtitle">  ${formatted_date}</span>
+                        <p class="card-title">${item.title}</p>
+                    </li>
+                    `
+                )
             })
         })
 
@@ -73,12 +72,16 @@ $(document).ready(function(){
 
 });
 
-// function selectBlog(title) {
-//     console.log(title)
-//     blog_data.items.forEach(item => {
-//         if (item.title == title) {
-//             $('#main-blog-title').text(item.title)
-//             $('#main-blog-content').html(item.content)
-//         }
-//     })
-// }
+function selectBlog(title) {
+    console.log(title)
+    blog_data.items.forEach(item => {
+        if (item.title == title) {
+            let temp_date = new Date(item.published)
+            let formatted_date = appendLeadingZeroes(temp_date.getDate()) + "/" + appendLeadingZeroes(temp_date.getMonth() + 1) + "/" + temp_date.getFullYear()
+            $(`#blog-container`).html(`
+                <h5 id="main-blog-title" >${item.title} <span class="blog-date"><p>${formatted_date}</p></span></h5>
+                <p id="main-blog-content" >${item.content}</p>
+            `)
+        }
+    })
+}
